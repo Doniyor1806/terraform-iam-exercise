a2b54a514e663ced4db8872fd676ee994f078548
Doniyor1806 <mydonis23@gmail.com>
2024-06-29 23:15:47 -0400

    added

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "group_name" {
+  description = "The name of the IAM group"
+  default     = "developers"
+}
+
+variable "user_name_1" {
+  description = "The name of the first IAM user"
+  default     = "developer_1"
+}
+
+variable "user_name_2" {
+  description = "The name of the second IAM user"
+  default     = "developer_2"
+}
