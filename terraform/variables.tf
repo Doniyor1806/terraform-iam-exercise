49985558717f0f2063709c451cb114dc19318058
Doniyor1806 <mydonis23@gmail.com>
2024-07-01 19:04:53 -0400

    variables modified

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,17 @@
 variable "group_name" {
   description = "The name of the IAM group"
-  default     = "developers"
+  type = string
+    default     = "developers"
 }
 
 variable "user_name_1" {
   description = "The name of the first IAM user"
-  default     = "developer_1"
+  type = string
+    default     = "developer_1"
 }
 
 variable "user_name_2" {
   description = "The name of the second IAM user"
-  default     = "developer_2"
+  type = string
+    default     = "developer_2"
 }
